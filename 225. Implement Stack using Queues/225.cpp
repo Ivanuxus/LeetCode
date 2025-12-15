9f43b69813b846ba564338fa6ecaeb8851af134a
Ivanuxus <119295581+Ivanuxus@users.noreply.github.com>
2025-12-15 12:58:22 +0300

    Update 225.cpp

--- 225. Implement Stack using Queues/225.cpp
+++ 225. Implement Stack using Queues/225.cpp
@@ -1,33 +1,70 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 class MyStack
 {
 public:
-    std::queue<int> q;
+    std::queue<int> q1;
+    std::queue<int> q2;
     MyStack()
     {
     }
 
     void push(int x)
     {
-        q.push(x);
+        q1.push(x);
     }
 
     int pop()
     {
-        int shit = q.back();
-        q.pop();
-        return shit;
+        while (q2.size() != 0)
+        {
+            q2.pop();
+        }
+        int tempPopped = 0;
+        int initSize = q1.size();
+        while (q1.size() != 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+            if (q1.size() == initSize - 1)
+            {
+                tempPopped = q2.back();
+            }
+        }
+        q1.swap(q2);
+        return q2.back();
     }
 
     int top()
     {
-        return q.back();
+        while (q2.size() != 0)
+        {
+            q2.pop();
+        }
+        int tempPopped = 0;
+        int initSize = q1.size();
+        while (q1.size() != 0)
+        {
+            q2.push(q1.front());
+            q1.pop();
+            if (q1.size() == 1)
+            {
+                tempPopped = q1.back();
+            }
+        }
+        tempPopped = q2.back();
+        q1.swap(q2);
+        while (q2.size() != 0)
+        {
+            q2.pop();
+        }
+        return tempPopped;
     }
 
     bool empty()
     {
-        if (q.size() == 0)
+        if (q1.size() == 0)
             return true;
         else
             return false;
@@ -39,6 +76,9 @@ int main()
     MyStack *obj = new MyStack();
     obj->push(1);
     obj->push(2);
-    int param_3 = obj->pop();
-    int param_2 = obj->top();
+    int param_3 = obj->top();
+    int param_2 = obj->pop();
+    int param_6 = obj->top();
+    int param_92 = obj->pop();
+    bool param_4 = obj->empty();
 }
